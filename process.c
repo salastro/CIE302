c5c603b91c7c711bea8f4416784e8188ffd3ea03
salastro <salah2112004@gmail.com>
2024-11-22 19:39:06 +0200

    Part IV Process
    Each process should act as if it is CPU-bound. Again, when a process
    finishes it should notify the scheduler on termination, the scheduler
    does NOT terminate the process.

--- process.c
+++ process.c
@@ -1,20 +1,50 @@
 #include "headers.h"
 
-/* Modify this file as needed*/
+int clk;
 int remainingtime;
+int termqid;
 
-int main(int agrc, char * argv[])
-{
-    initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
-    {
-        // remainingtime = ??;
+// Handle the command line arguments
+void parseArgs(int argc, char *argv[]) {
+    remainingtime = atoi(argv[1]);
+    if (remainingtime <= 0) {
+        perror("Invalid remaining time");
+        exit(-1);
     }
-    
+}
+
+// Simulating CPU-bound behavior
+void simulateProcess() {
+    while (remainingtime > 0) {
+        // Consume the remaining time only if the clock has ticked
+        clk = getClk();
+        while (clk == getClk());
+        remainingtime--;
+    }
+}
+
+// Notify the scheduler upon completion
+void notifyScheduler() {
+    termqid = initMsgq(termkey);
+    if (termqid == -1) {
+        perror("Error in initializing termination queue");
+        exit(-1);
+    }
+    termination_t term;
+    term.mtype = 1;
+    term.process_id = getpid();
+    int send_val = msgsnd(termqid, &term, sizeof(term), 0);
+    if (send_val == -1) {
+        perror("Error in sending the termination message");
+        exit(-1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    parseArgs(argc, argv);
+    initClk();
+    simulateProcess();
+    notifyScheduler();
     destroyClk(false);
-    
-    return 0;
+    exit(0);
 }
