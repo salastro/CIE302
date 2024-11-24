2d8ac827720ec7d35c75a53290c67db825618010
salastro <salah2112004@gmail.com>
2024-11-24 17:07:01 +0200

    Scheduler Implementation
    - Handle children
    - Handle child termination
    - SJF

--- scheduler.c
+++ scheduler.c
@@ -2,57 +2,109 @@
 
 int clk;
 int msqid;
+process_t running = {
+    .id = -1,
+    .arrival = -1,
+    .runtime = -1,
+    .priority = -1,
+    .remainingTime = -1,
+    .running = false,
+    .pid = -1,
+};
+
 
 pid_t startProcess(process_t process) {
     process.pid = fork();
-    char remainingTime[5];
-    sprintf(remainingTime, "%d", process.runtime);
-    char *args[] = {"./process.out", remainingTime, NULL};
-    int proc_exec = execvp(args[0], args);
-    if (proc_exec == -1) {
-        perror("Error in executing process");
+    if (process.pid == -1) {
+        perror("Error in forking process");
         exit(-1);
+    }
+    if (process.pid == 0) {
+        char remainingTime[10];
+        sprintf(remainingTime, "%d", process.runtime);
+        char id[10];
+        sprintf(id, "%d", process.id);
+        char *args[] = {"./process.out", id, remainingTime, NULL};
+        int proc_exec = execvp(args[0], args);
+        if (proc_exec == -1) {
+            perror("Error in executing process");
+            exit(-1);
+        }
+        process.running = true;
+        exit(0);
     }
     return process.pid;
 }
 
-void stopProcess(int pid) {
-    kill(pid, SIGSTOP);
+int stopProcess(int pid) {
+    int status = kill(pid, SIGSTOP);
+    if (status == -1) {
+        perror("Error in stopping process");
+        exit(-1);
+    }
+    return status;
 }
 
-void continueProcess(int pid) {
-    kill(pid, SIGCONT);
+int continueProcess(int pid) {
+    int status = kill(pid, SIGCONT);
+    if (status == -1) {
+        perror("Error in continuing process");
+        exit(-1);
+    }
+    return status;
+}
+
+void handleTermination(int signum) {
+    running.running = false;
+    printf("Process ID %d terminated at time %d\n", running.id, getClk());
 }
 
 void sjf();
 void phpf();
 void rr();
 
+// Shortest Job First
 void sjf() {
     // Initialize the priority queue
     Node *head;
     initializePQ(&head);
-    process_t process;
-    process.id = 0;
-    int pid;
+    process_t incoming;
+    pid_t pid;
+    bool last = false;
 
+    // Loop until there are no more processes
     while (true) {
-        // Receive a process from the message queue
-        msgrcv(msqid, &process, sizeof(process), 0, !IPC_NOWAIT);
-
-        // Push the process into the priority queue
-        push(&head, process, process.runtime);
-
-        // If the priority queue is not empty, start the process
-        if (!isEmptyPQ(&head)) {
-            process = pop(&head);
-            startProcess(process);
+        if (!last) {
+            // Receive a new process
+            incoming = receiveMsg(msqid);
+            if (incoming.id == -1)
+                last = true;
+            // Push the new process into the priority queue
+            printf("Received process with ID %d and runtime %d\n", incoming.id, incoming.runtime);
+            push(&head, incoming, incoming.runtime);
+        }
+        if (!isEmptyPQ(&head) && !running.running) {
+            // Pop the process with the shortest runtime
+            running = pop(&head);
+            if (running.id == -1)
+                return;
+            running.running = true;
+            pid = startProcess(running);
         }
     }
 }
 
+// Preemptive Highest Priority First
+void phpf() {}
+
+// Round Robin
+void rr() {}
+
+
 int main(int argc, char * argv[])
 {
+    // Initialize the clock
+    printf("Scheduler started PID %d\n", getpid());
     initClk();
 
     // Arguments
@@ -61,6 +113,9 @@ int main(int argc, char * argv[])
 
     // Initialize the message queue
     msqid = initMsgq(msgkey);
+
+    // Set the signal handler
+    signal(SIGCHLD, handleTermination);
 
     switch (algorithm) {
         case SJF:
